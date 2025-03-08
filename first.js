947bf4da2bf16dd91c76c8a7d61892f3d68eea42
ankit-yadav90 <ankityadav73699@gmail.com>
2025-03-08 12:57:43 +0530

    prompt use prectice

---
+++ first.js
@@ -0,0 +1,9 @@
+//Conditinal Statement
+
+let num = prompt("enter anumber");
+
+if (num % 5 === 0) {
+    console.log(num, "is a multiple of 5");
+} else{
+    console.log(num, "is a Not multiple of 5");
+}
